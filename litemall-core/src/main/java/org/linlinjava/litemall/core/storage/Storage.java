d6b6ab4b493e74f7d52edd53ed32aeb6334eddf5
marlio <jitao.liu82@gmail.com>
2020-03-14 12:41:40 +0800

    updated recently.

--- litemall-core/src/main/java/org/linlinjava/litemall/core/storage/Storage.java
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/storage/Storage.java
@@ -1,3 +1,4 @@
+
 package org.linlinjava.litemall.core.storage;
 
 import org.springframework.core.io.Resource;
@@ -11,23 +12,18 @@ import java.util.stream.Stream;
  */
 public interface Storage {
 
-    /**
-     * 存储一个文件对象
-     *
-     * @param inputStream   文件输入流
-     * @param contentLength 文件长度
-     * @param contentType   文件类型
-     * @param keyName       文件名
-     */
-    void store(InputStream inputStream, long contentLength, String contentType, String keyName);
-
-    Stream<Path> loadAll();
-
-    Path load(String keyName);
-
-    Resource loadAsResource(String keyName);
-
-    void delete(String keyName);
-
-    String generateUrl(String keyName);
+	/**
+	 * 存储一个文件对象
+	 *
+	 * @param inputStream   文件输入流
+	 * @param contentLength 文件长度
+	 * @param contentType   文件类型
+	 * @param keyName       文件名
+	 */
+	void store(InputStream inputStream, long contentLength, String contentType, String keyName);
+	Stream<Path> loadAll();
+	Path load(String keyName);
+	Resource loadAsResource(String keyName);
+	void delete(String keyName);
+	String generateUrl(String keyName);
 }
